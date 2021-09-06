eaac117ecba956e5c9b6235145598e2861ce1ebc
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-06 09:11:00 -0300

    More accs

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
 
 app.get('/:nome/:lang', (req, res) => {
   const nome = "Gabriel";
   const lang = "Javascript";
   const showMsg = false;
 
+  const products = [
+    { product: 'Doritos', price: 3.4 },
+    { product: 'Coke', price: 5 },
+    { product: 'Milk', price: 10 },
+  ]
+
   res.render('index', {
     nome,
     lang,
-    msg: showMsg
+    msg: showMsg,
+    products
   });
 })
 
