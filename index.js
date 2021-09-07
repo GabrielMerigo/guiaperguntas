d580b17b961e07d79bdc62212ebb2e21c7111c05
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-07 12:52:16 -0300

    Adicioando formulario

--- index.js
+++ index.js
@@ -3,23 +3,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.get('/:nome/:lang', (req, res) => {
-  const nome = "Gabriel";
-  const lang = "Javascript";
-  const showMsg = false;
+app.get('/', (req, res) => {
 
-  const products = [
-    { product: 'Doritos', price: 3.4 },
-    { product: 'Coke', price: 5 },
-    { product: 'Milk', price: 10 },
-  ]
+  res.render("index");
+})
 
-  res.render('index', {
-    nome,
-    lang,
-    msg: showMsg,
-    products
-  });
+app.get('/perguntar', (req, res) => {
+  res.render("perguntar");
 })
 
 app.listen(8080, () => {
